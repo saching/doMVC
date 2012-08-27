5ace0287ed4b52d403d058557e983383b4209236
Sachin Gosarade <saching@siddhatech.com>
2012-08-27 19:00:18 +0530

    updated framework directory structure

--- public/index.php
+++ public/index.php
@@ -14,4 +14,4 @@ define('ROOT', dirname(dirname(__FILE__)));
 
 $url = array_key_exists('url', $_GET)?$_GET['url']:'';
 
-require_once (ROOT . DS . 'library' . DS . 'bootstrap.php');
+require_once (ROOT . DS . 'library' . DS . 'framework' . DS . 'bootstrap.php');
